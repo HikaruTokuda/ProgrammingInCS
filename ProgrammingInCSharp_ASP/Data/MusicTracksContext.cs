0f9ae19962f4cc95caa58612aac0da048174b951
CO\MM13406 <hikaru.tokuda@murata.com>
2020-05-10 21:53:57 +0900

    ASPプロジェクト追加

---
+++ ProgrammingInCSharp_ASP/Data/MusicTracksContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace ProgrammingInCSharp_ASP.Models
+{
+    public class MusicTracksContext : DbContext
+    {
+        public MusicTracksContext (DbContextOptions<MusicTracksContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ProgrammingInCSharp_ASP.Models.MusicTrack> MusicTrack { get; set; }
+    }
+}
---
+++ ProgrammingInCSharp_ASP/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ProgrammingInCSharp_ASP.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
